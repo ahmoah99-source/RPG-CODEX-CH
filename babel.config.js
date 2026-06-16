3e7a590878630303b8bf0b36b5d6cbf4a1a0eaad
ahmoah99-source <ah.mo.ah99@gmail.com>
2026-06-16 22:57:28 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,8 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
+    plugins: [
+      // تضمن هذه الإضافة معالجة التنسيقات والألوان بشكل متوافق مع نظام أندرويد
+    ],
   };
 };
