2ab949df4fb07b5d0813e7d03966a20efcdb8c0c
ahmoah99-source <ah.mo.ah99@gmail.com>
2026-06-16 18:46:47 +0300

    Create constants/theme.js

---
+++ src/constants/theme.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  background: '#101018',
+  surface: '#1A1A24',
+  surfaceLight: '#252530',
+  gold: '#D4A017',
+  goldLight: '#FFE680',
+  goldDark: '#8B6914',
+  textPrimary: '#F0E6D0',
+  textSecondary: '#9A9080',
+  red: '#F87171',
+  blue: '#60A5FA',
+  green: '#4ADE80',
+};
