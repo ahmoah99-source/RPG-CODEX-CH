cded010516225f5c040b4b861b20d7b330ee6351
ahmoah99-source <ah.mo.ah99@gmail.com>
2026-06-20 18:16:35 +0300

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,12 +1,21 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// الشاشات الأساسية
 import DashboardScreen from '../screens/DashboardScreen';
 import DetailScreen from '../screens/DetailScreen';
 import CreateScreen from '../screens/CreateScreen';
 import AddItemScreen from '../screens/AddItemScreen';
-// إضافة استيراد صفحات المستويات الجديدة
+
+// شاشات المستويات
 import LevelsListScreen from '../screens/LevelsListScreen';
 import AddLevelScreen from '../screens/AddLevelScreen';
+
+// شاشات المواهب والرتب
+import TalentsListScreen from '../screens/TalentsListScreen';
+import AddTalentScreen from '../screens/AddTalentScreen';
+import RanksListScreen from '../screens/RanksListScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,13 +29,20 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={screenOptions}>
+        {/* الشاشات الأساسية */}
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
         <Stack.Screen name="Create" component={CreateScreen} />
         <Stack.Screen name="AddItem" component={AddItemScreen} />
-        {/* تسجيل صفحات المستويات في الـ Navigator */}
+        
+        {/* شاشات المستويات */}
         <Stack.Screen name="LevelsList" component={LevelsListScreen} />
         <Stack.Screen name="AddLevel" component={AddLevelScreen} />
+        
+        {/* شاشات المواهب والرتب */}
+        <Stack.Screen name="TalentsList" component={TalentsListScreen} />
+        <Stack.Screen name="AddTalent" component={AddTalentScreen} />
+        <Stack.Screen name="RanksList" component={RanksListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
