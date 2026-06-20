d8290baa1cbc692b6634298d0d08335c588e0053
ahmoah99-source <ah.mo.ah99@gmail.com>
2026-06-20 16:37:04 +0300

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,6 +4,9 @@ import DashboardScreen from '../screens/DashboardScreen';
 import DetailScreen from '../screens/DetailScreen';
 import CreateScreen from '../screens/CreateScreen';
 import AddItemScreen from '../screens/AddItemScreen';
+// إضافة استيراد صفحات المستويات الجديدة
+import LevelsListScreen from '../screens/LevelsListScreen';
+import AddLevelScreen from '../screens/AddLevelScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +24,9 @@ export default function AppNavigator() {
         <Stack.Screen name="Detail" component={DetailScreen} />
         <Stack.Screen name="Create" component={CreateScreen} />
         <Stack.Screen name="AddItem" component={AddItemScreen} />
+        {/* تسجيل صفحات المستويات في الـ Navigator */}
+        <Stack.Screen name="LevelsList" component={LevelsListScreen} />
+        <Stack.Screen name="AddLevel" component={AddLevelScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
